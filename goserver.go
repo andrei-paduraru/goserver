667e43fdf9e49f0b3d7107d89d8b9c16da4d99ab
Andrei Paduraru <andrei.paduraru@qualitance.ro>
2015-07-17 17:09:39 +0300

    Inject http Request and ResponseWriter into methods

--- goserver.go
+++ goserver.go
@@ -17,25 +17,31 @@ func main() {
 	http.ListenAndServe(port, nil)
 }
 
-func getEntry() {
+func getEntry(res http.ResponseWriter, req *http.Request) {
     fmt.Println("Get entry")
+    fmt.Println(req.URL)
 }
 
-func postEntry() {
+func postEntry(res http.ResponseWriter, req *http.Request) {
     fmt.Println("Post entry")
+    fmt.Println(req.URL)
 }
 
 type ReqHandler struct {
     path string
 }
 
-func (r *ReqHandler) handle(get func(), post func(), put func(), delete func()) {
+func (r *ReqHandler) handle(
+        get    func(res http.ResponseWriter, req *http.Request),
+        post   func(res http.ResponseWriter, req *http.Request),
+        put    func(res http.ResponseWriter, req *http.Request),
+        delete func(res http.ResponseWriter, req *http.Request)) {
     http.HandleFunc(r.path, func(res http.ResponseWriter, req *http.Request) {
         m := req.Method
         var status int
         status = http.StatusOK
         fmt.Printf("%s %s ", m, r.path)
-        var call func()
+        var call func(res http.ResponseWriter, req *http.Request)
         switch m {
             case "GET": if get != nil {call = get}else{status = http.StatusNotFound}
             case "POST": if post != nil {call = post}else{status = http.StatusNotFound}
@@ -46,7 +52,7 @@ func (r *ReqHandler) handle(get func(), post func(), put func(), delete func()) 
         if status == http.StatusNotFound {
             http.Error(res, "Not found", http.StatusNotFound)
         }else{
-            call()
+            call(res, req)
         }
     })
 }
