36ee2200f02df792223600d40c0aa43c88d7b8f5
Andrei Paduraru <andrei.paduraru@qualitance.ro>
2015-07-17 14:20:48 +0300

    A very basic request handler

--- goserver.go
+++ goserver.go
@@ -9,6 +9,33 @@ func main() {
 	fs := http.FileServer(http.Dir("public"))
   	http.Handle("/", fs)
 
-	http.ListenAndServe(":8080", nil)
-  	fmt.Printf("Listening...")
+    testHandler := ReqHandler{ path: "/entry" }
+    testHandler.handle(getEntry, postEntry)
+
+    port := ":8080"
+  	fmt.Printf("Listening on %s...\n", port)
+	http.ListenAndServe(port, nil)
+}
+
+func getEntry() {
+    fmt.Println("Get entry")
+}
+
+func postEntry() {
+    fmt.Println("Post entry")
+}
+
+type ReqHandler struct {
+    path string
+}
+
+func (r *ReqHandler) handle(get func(), post func()) {
+    http.HandleFunc(r.path, func(res http.ResponseWriter, req *http.Request) {
+        m := req.Method
+        fmt.Printf("Method %q\n", m)
+        switch m {
+            case "GET": get()
+            case "POST": post()
+        }
+    })
 }
